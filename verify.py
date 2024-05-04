67730c35fd19201f7b9d377a8579cb16fd87f702
W20MC <157727813+W20MC@users.noreply.github.com>
2024-05-04 11:16:11 +0200

    Add files via upload

---
+++ verify.py
@@ -0,0 +1,7 @@
+import sys
+import requests
+
+author = sys.argv[1]
+
+config = requests.get("https://raw.githubusercontent.com/iArtie/texture-shop-index/main/config.json").json()
+print("Yes" if author in config["verified"] else "No")
