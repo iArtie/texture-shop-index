4a35f3715e6ab6516f58390edb0f55a5a8279f1b
W20MC <157727813+W20MC@users.noreply.github.com>
2024-05-04 10:55:16 +0200

    Create test_bot.py

---
+++ .github/workflows/test_bot.py
@@ -0,0 +1,17 @@
+import json
+import requests
+import os
+
+headers = {
+    "Accept": "application/vnd.github+json",
+    "Authorization": f"Bearer {os.environ["TOKEN"]}",
+    "X-GitHub-Api-Version": "2022-11-28"
+}
+
+data = {
+    "body": "Accepted texture pack!"
+}
+
+req = requests.post("https://api.github.com/repos/iArtie/texture-shop-index/issues/3/comments", headers = headers, data = json.dumps(data))
+
+print(req.text)
