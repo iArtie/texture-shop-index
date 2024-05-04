1af6eb3d92b6eab0390dc038b076063a474a9627
W20MC <157727813+W20MC@users.noreply.github.com>
2024-05-04 13:33:46 +0200

    Update upload_folder.py

--- upload_folder.py
+++ upload_folder.py
@@ -5,9 +5,9 @@ import re
 import requests
 
 folders_list = []
-url = re.search(r'\((.*?)\)', os.environ["BODY"]).group(1)
-req = requests.get(url)
-
+url = re.search(r'\((.*?)\)', os.environ["BODY"])
+print(os.environ["BODY"])
+req = requests.get("https://download1530.mediafire.com/1bx3q8togpzgLBNwddvUGAN_C55mKAcf9Fi7pWVGSfmM_VHOLPDOnUUYuxzsfIGFjr0mA4979H8JD694ot8SKZQQWwZZ9hausHLSD-ErivyxuzmIa3XAk2aEnmCd6ulQkE4a0-dD4ZpROih_FFwUDs0IWn5z6D_fKh1Fkrbp91o/5673zhc0deidsgp/METAL.zip")
 # Open the ZIP file
 with zipfile.ZipFile(BytesIO(req.content)) as zip_ref:
     # Iterate through files in the ZIP archive
