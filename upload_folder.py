a51b02c7e280a223e8c490e78ad5f30cf851c5b0
W20MC <157727813+W20MC@users.noreply.github.com>
2024-05-04 13:26:27 +0200

    Create upload_folder.py

---
+++ upload_folder.py
@@ -0,0 +1,31 @@
+from io import BytesIO
+import os
+import zipfile
+import re
+import requests
+
+folders_list = []
+req = requests.get("temp")
+
+# Open the ZIP file
+with zipfile.ZipFile(BytesIO(req.content)) as zip_ref:
+    # Iterate through files in the ZIP archive
+    for file_info in zip_ref.filelist:
+        # Extract the filename
+        filename = file_info.filename
+        # Match folder names using regular expression
+        folder_match = re.search(r'.*/$', filename)
+        if folder_match:
+            # Extract the matched folder name
+            folder_name = folder_match.group()
+            folders_list.append(folder_name)
+    
+    try:
+        if folders_list[0] != re.match(r"^[^/]+/$", folders_list[0]):
+            zip_ref.extractall()
+    except IndexError:
+        folder = filename.replace(".zip", "")
+        if not os.path.exists(folder):
+            os.mkdir(folder)
+        
+        zip_ref.extractall(folder)
